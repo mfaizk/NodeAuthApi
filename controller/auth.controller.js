e4ff797ce3fd30d9655c77fd8818f3cc43c61a9c
mfaizk <suneater1413@gmail.com>
2022-11-07 17:28:04 +0530

    password hashing introduced

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,26 +1,43 @@
 const User = require("../model/user");
 const { SECRET } = process.env;
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const signUp = async (req, res) => {
   const { fname, lname, email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+
   if (!(fname && lname && email && password)) {
     res.status(400).send("Data is missing");
   }
   if (!(await User.findOne({ email }))) {
-    const user = await User.create({ fname, lname, email, password });
-    // console.log(user);
+    const hashPassword = await bcrypt.hash(password, salt);
+    // console.log(hashPassword);
+    const user = await User.create({
+      fname,
+      lname,
+      email,
+      password: hashPassword,
+    });
+    user.password = undefined;
     res.send({ user });
   } else {
-    res.send("Data already exist in db");
+    res.send("User already exist in db");
   }
 };
 
 const signIn = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
-  if (user && user.password === password) {
-    res.send(jwt.sign({ email }, SECRET));
+  // console.log(await bcrypt.compare(password, user.password));
+
+  if (user) {
+    if (await bcrypt.compare(password, user.password)) {
+      res.send(jwt.sign({ email }, SECRET));
+    } else {
+      res.send("password isn't correct");
+    }
   } else {
     res.send("user doesn't exist");
   }
